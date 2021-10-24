bc61dd21e82f47c9f4d5d8946cd6d17cfda89d9e
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-10-24 13:30:52 +0200

    no comments

--- SendGMail/Program.cs
+++ SendGMail/Program.cs
@@ -10,8 +10,6 @@ namespace SendGMail
 {
     class Program
     {
-        static bool mailSent = false;
-
         static void Main()
         {
             var mailService = new SendGMailService(
@@ -22,55 +20,6 @@ namespace SendGMail
                 $"Sended at: {DateTime.Now.ToUniversalTime()} via GMail"
                 );
             mailService.Send();
-        }
-        //static void Main(string[] args)
-        //{
-        //    var fromAddress = new MailAddress("sergey.davidovich@gmail.com", "From Sergey D");
-        //    var toAddress = new MailAddress("writesd@hotmail.com", "To SD");
-
-        //    const string fromPassword = "ksenjuk57NBgo"; // Insert your GMail password here
-        //    const string subject = "Test Subject";
-
-        //    string body = $"Sended at: {DateTime.Now.ToUniversalTime()} via GMail";
-
-        //    var smtp = new SmtpClient
-        //    {
-        //        Host = "smtp.gmail.com",
-        //        Port = 587,
-        //        DeliveryMethod = SmtpDeliveryMethod.Network,
-        //        EnableSsl = true,
-        //        UseDefaultCredentials = false,
-        //        Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
-        //    };
-        //    using (var message = new MailMessage(fromAddress, toAddress)
-        //    {
-        //        Subject = subject,
-        //        Body = body
-        //    })
-
-        //    {
-        //        smtp.Send(message);
-        //    }
-        //}
-
-        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
-        {
-            // Get the unique identifier for this asynchronous operation.
-            String token = (string)e.UserState;
-
-            if (e.Cancelled)
-            {
-                Console.WriteLine("[{0}] Send canceled.", token);
-            }
-            if (e.Error != null)
-            {
-                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
-            }
-            else
-            {
-                Console.WriteLine("Message sent.");
-            }
-            mailSent = true;
         }
     }
 }
