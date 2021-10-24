b5c0ea00a74443e74b65e3143ec1627373585ae5
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-10-24 15:13:45 +0200

    no comments

--- SendGMail/Program.cs
+++ SendGMail/Program.cs
@@ -15,7 +15,7 @@ namespace SendGMail
             var mailService = new SendGMailService(
                 "sergey.davidovich@gmail.com",
                 "writesd@hotmail.com",
-                "",
+                "password",
                 "Test Subject",
                 $"Sended at: {DateTime.Now.ToUniversalTime()} via GMail"
                 );
